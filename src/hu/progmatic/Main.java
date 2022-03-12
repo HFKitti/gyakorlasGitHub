c94a98c83230e0afba98d45e5828ec06d2eb849e
Nagyvaradi Adam <adam.nagyvaradi@gmail.com>
2022-03-12 15:12:44 +0100

    1 feladat

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -3,6 +3,21 @@ package hu.progmatic;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int year = 1999;
+        if (year > 1582) {
+            if (year % 4 == 0 && year % 400 == 0) {
+                System.out.println("Szökőév");
+            } else if (year % 100 == 0){
+                System.out.println("Nem szökőév");
+            } else if (year % 4 == 0) {
+                System.out.println("Szökőév");
+            } else {
+                System.out.println("Nem szökőév");
+            }
+        }   else {
+            System.out.println("Nem szökőév");
+        }
+
     }
 }
